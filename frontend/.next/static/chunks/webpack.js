5eb4bc8505118ec7e306c63aa05b0fb80cc83843
Alex Luo <alexbluo01@gmail.com>
2022-09-28 15:33:36 -0400

    clean up gitignore:

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3de2b256ee1485c0"; }
+/******/ 		__webpack_require__.h = function() { return "8836dbafef7f24b8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
