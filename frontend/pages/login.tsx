3d52a5c891999fba9f7b0f352567bf48137e2725
Evan Marlow <itsarlow@gmail.comgit config --global user.email itsarlow@gmail.com>
2022-12-19 08:42:07 -0500

    idk

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,7 +1,7 @@
-import { NextPage } from "next"
+import { NextPage } from "next";
 
 const Login: NextPage = () => {
-  return <div className=""></div>
-}
+  return <div className=""> Sup</div>;
+};
 
 export default Login;
