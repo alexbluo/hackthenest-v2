f44321e1bc59952f24e09561b8b0cbd761280882
Alex Luo <alexbluo01@gmail.com>
2022-12-20 08:28:50 -0500

    no idea

---
+++ components/AboutSection.tsx
@@ -0,0 +1,22 @@
+const AboutSection = () => {
+  return (
+    <section className="bg-yellow-400 h-screen" id="about">
+      
+        <div className= "text-center text-8xl p-10">
+          About
+        </div>
+
+        <div className = "grid grid-cols-2">
+
+          <div className = "text-center"> soerjf</div>
+          <div className = "text-center">andanwd</div>
+          
+        </div>
+      
+    </section>
+
+    
+  );
+};
+
+export default AboutSection;
