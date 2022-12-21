f6ba49ec7f62c64dd465a038d37965722fe32fbc
Evan Marlow <itsarlow@gmail.comgit config --global user.email itsarlow@gmail.com>
2022-12-21 08:41:28 -0500

    add faq

---
+++ components/FAQ.tsx
@@ -0,0 +1,10 @@
+import { useState} from "react"
+
+const FAQ = () => {
+    const {open, setOpen} = useState<boolean>(false); 
+
+    
+    return <div>testlesktseionts</div>
+}
+
+export default FAQ;
--- components/FAQSection.tsx
+++ components/FAQSection.tsx
@@ -1,8 +1,24 @@
+import FAQ from "./FAQ";
+
 const FAQSection = () => {
-
-
-
-  return <section className="bg-purple-800 h-screen" id="faq"><div>hi</div></section>;
+  return (
+    <section className="bg-purple-800 h-screen" id="faq">
+      <h1>tests</h1>
+      <div className="grid grid-cols-2">
+        <FAQ></FAQ>
+        <FAQ></FAQ>
+        <FAQ></FAQ>
+        <FAQ></FAQ>
+        <FAQ></FAQ>
+        <FAQ></FAQ>
+        <FAQ></FAQ>
+        <FAQ></FAQ>
+        <FAQ></FAQ>
+        <FAQ></FAQ>
+        <FAQ></FAQ>
+      </div>
+    </section>
+  );
 };
 
 export default FAQSection;
