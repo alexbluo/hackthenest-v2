b76085ac1ade3ef85d872d5ac57131c714b7eece
tyler nguyen <tylernguyen@tylers-MacBook-Pro.local>
2022-12-21 08:16:00 -0500

    add faq

---
+++ components/FAQ.tsx
@@ -0,0 +1,12 @@
+const FAQ = () => {
+    
+  return (
+    <section className="bg-purple-800 h-screen" id="faq">
+      <div>
+        faq
+      </div>
+    </section>
+  );
+};
+
+export default FAQ;
