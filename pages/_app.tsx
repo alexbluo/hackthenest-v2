060dbf89d31ae499ee8a792ac97876a60484a0f5
Alex Luo <alexbluo01@gmail.com>
2022-12-30 21:37:12 -0500

    add google analytics:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,46 @@
+import { useEffect } from "react";
 import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import * as gtag from "../utils/gtag";
+import { GA_TRACKING_ID } from "../utils/gtag";
 import "../index.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
         <link rel="icon" href="/logo-colored.png" />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
       <Analytics />
