18a5c132c89e11ad700615796937bd1467359e48
Evan Marlow <itsarlow@gmail.comgit config --global user.email itsarlow@gmail.com>
2023-01-02 17:27:25 -0500

    add sponsors

---
+++ components/Sponsor.tsx
@@ -0,0 +1,19 @@
+import classNames from "classnames";
+import Image from "next/image";
+
+interface Props {
+  path: string;
+  alt: string;
+}
+
+const Sponsor = ({ path, alt }: Props) => {
+  return (
+    <div className="h-48 w-full bg-orange p-10" id="sponsors">
+      <div className="relative h-full">
+        <Image src={path} alt={alt} fill />
+      </div>
+    </div>
+  );
+};
+
+export default Sponsor;
--- components/SponsorsSection.tsx
+++ components/SponsorsSection.tsx
@@ -1,7 +1,23 @@
+import Sponsor from "./Sponsor";
+
 const SponsorsSection = () => {
   return (
     <section className="h-screen bg-grey" id="sponsors">
       <h2>Sponsors</h2>
+      <div className="flex flex-col gap-4">
+        <div className="flex">
+          <Sponsor path="/evan.jpg" alt="evan"/>
+        </div>
+        <div className="flex gap-4 ">
+          <Sponsor path="/evan.jpg" alt="evan"/>
+          <Sponsor path="/evan.jpg" alt="evan"/>
+        </div>
+        <div className="flex gap-4">
+          <Sponsor path="/evan.jpg" alt="evan"/>
+          <Sponsor path="/evan.jpg" alt="evan"/>
+          <Sponsor path="/evan.jpg" alt="evan"/>
+        </div>
+      </div>
     </section>
   );
 };
