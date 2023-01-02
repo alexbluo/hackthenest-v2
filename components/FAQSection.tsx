f9a05d9ac5ffe4b6b44d437cd8a44bd6b226d565
Tyler Nguyen <tytyprime@gmail.com>
2023-01-02 16:25:17 -0500

    separate faq columns

--- components/FAQSection.tsx
+++ components/FAQSection.tsx
@@ -4,17 +4,19 @@ const FAQSection = () => {
   return (
     <section className="h-screen bg-orange text-6xl" id="faq">
       <h2 className="text-center">FAQ</h2>
-      <div className="grid grid-cols-2 gap-4">
-        <FAQ question="A">A contents</FAQ>
-        <FAQ question="B">B contents</FAQ>
-        <FAQ question="C">C contents</FAQ>
-        <FAQ question="D">D contents</FAQ>
-        <FAQ question="E">E contents</FAQ>
-        <FAQ question="F">F contents</FAQ>
-        <FAQ question="G">G contents</FAQ>
-        <FAQ question="H">H contents</FAQ>
-        <FAQ question="I">I contents</FAQ>
-        <FAQ question="J">J contents</FAQ>
+      <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col gap-4 w-full">
+          <FAQ question="A">A contents</FAQ>
+          <FAQ question="B">B contents</FAQ>
+          <FAQ question="C">C contents</FAQ>
+          <FAQ question="D">D contents</FAQ>
+        </div>
+        <div className="flex flex-col gap-4 w-full">
+          <FAQ question="E">E contents</FAQ>
+          <FAQ question="F">F contents</FAQ>
+          <FAQ question="G">G contents</FAQ>
+          <FAQ question="H">H contents</FAQ>
+        </div>
       </div>
     </section>
   );
