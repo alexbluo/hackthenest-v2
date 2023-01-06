6f85cd084a7c81e6a7d8c286f2abebea253b000b
Alex Luo <alexbluo01@gmail.com>
2023-01-06 09:07:59 -0500

    add optional sponsor href

--- components/Sponsor.tsx
+++ components/Sponsor.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 
 interface Props {
+  href?: string;
   path: string;
   alt: string;
 }
 
-const Sponsor = ({ path, alt }: Props) => {
+const Sponsor = ({ href = "", path, alt }: Props) => {
   return (
-    <div className="h-48 w-full p-10 bg-blue-dark " id="sponsors">
-      <div className="relative h-full">
-        <Image src={path} alt={alt} fill style={{objectFit: "contain"}}  />
-      </div>
+    <div className="h-48 w-full bg-blue-dark p-10" id="sponsors">
+      <a href={href}>
+        <div className="relative h-full">
+          <Image src={path} alt={alt} fill style={{ objectFit: "contain" }} />
+        </div>
+      </a>
     </div>
   );
 };
