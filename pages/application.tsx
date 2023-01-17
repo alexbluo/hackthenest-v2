68eeac4e61b29bb37ba27637b197c0e9cb96f962
Alex Luo <alexbluo01@gmail.com>
2023-01-16 21:15:19 -0500

    clean up application

--- pages/application.tsx
+++ pages/application.tsx
@@ -1,11 +1,29 @@
-import { useForm } from "react-hook-form";
 import { useState } from "react";
-import { ErrorMessage } from "@hookform/error-message";
 import Dropdown from "react-dropdown";
+import { useForm } from "react-hook-form";
 import "react-dropdown/style.css";
-import { optionCSS } from "react-select/dist/declarations/src/components/Option";
+
+// TODO: move options to inline
+// fix other - search up
+// reduce spacing
+// styles
+// types
+// validation
+// - email, phone number, types, etc.
+// checkboxes for mlh + links + wording
 
 const Application = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   const ageOptions = ["13", "14", "15", "16", "17", "18"];
 
   const roleOptions = ["Hacker", "Volunteer", "Mentor", "Chaperone"];
@@ -14,7 +32,7 @@ const Application = () => {
     "Vegan",
     "Vegetarian",
     "Peanut allergies",
-    <input placeholder="Other..."></input>,
+    <input placeholder="Other..." />,
   ];
 
   const whereOptions = [
@@ -23,33 +41,16 @@ const Application = () => {
     "Instagram",
     "Twitter",
     "Facebook",
-    <input placeholder="Other..."></input>,
+    <input placeholder="Other..." />,
   ];
   const agreeOptions = ["Yes", "No"];
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    watch,
-  } = useForm();
-
-  
-
-  const [age, setAge] = useState("");
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
-  console.log(watch("firstName"));
 
   return (
     <section className="h-[160rem]">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="flex gap-28 py-20 px-40  text-xl" id="names">
           <div className="w-1/2 flex-col">
-            <div className="relative font-bold">What is your first name? *</div>
+            <div className="relative font-bold">First Name *</div>
 
             <input
               className="bg-transparent h-10 w-full rounded shadow"
@@ -59,14 +60,14 @@ const Application = () => {
                 required: true,
               })}
               aria-invalid={errors.firstName ? "true" : "false"}
-            ></input>
+            />
             {errors.firstName?.type === "required" && (
               <p role="alert">This field is required</p>
             )}
           </div>
 
           <div className="w-1/2 flex-col">
-            <div className="relative font-bold">What is your last name? *</div>
+            <div className="relative font-bold">Last Name *</div>
 
             <input
               className="bg-transparent h-10 w-full rounded shadow"
@@ -76,7 +77,7 @@ const Application = () => {
                 required: true,
               })}
               aria-invalid={errors.lastName ? "true" : "false"}
-            ></input>
+            />
             {errors.lastName?.type === "required" && (
               <p role="alert">This field is required</p>
             )}
@@ -84,7 +85,7 @@ const Application = () => {
         </div>
 
         <div className="flex-col gap-28 py-20 px-40  text-xl" id="email">
-          <div className="relative font-bold">What is your email? *</div>
+          <div className="relative font-bold">Email *</div>
 
           <input
             className="bg-transparent h-10 w-full rounded shadow"
@@ -94,14 +95,14 @@ const Application = () => {
               required: true,
             })}
             aria-invalid={errors.email ? "true" : "false"}
-          ></input>
+          />
           {errors.email?.type === "required" && (
             <p role="alert">This field is required</p>
           )}
         </div>
 
         <div className="flex-col gap-28 py-20 px-40  text-xl" id="email">
-          <div className="relative font-bold">What is your phone number? *</div>
+          <div className="relative font-bold">Phone Number *</div>
 
           <input
             className="bg-transparent h-10 w-full rounded shadow"
@@ -111,64 +112,60 @@ const Application = () => {
               required: true,
             })}
             aria-invalid={errors.phoneNumber ? "true" : "false"}
-          ></input>
+          />
           {errors.phoneNumber?.type === "required" && (
             <p role="alert">This field is required</p>
           )}
         </div>
 
         <div className="flex-col gap-28 py-20 px-40  text-xl" id="email">
-          <div className="relative font-bold" >What is your age? *</div>
+          <div className="relative font-bold">Age? *</div>
 
           <Dropdown
-          options={ageOptions}
+            options={ageOptions}
             placeholder="Select an option..."
             className="w-1/3 rounded"
-            
-            /*{...register("age", {
+
+            /* {...register("age", {
               required: true,
             })}
             aria-invalid={errors.age ? "true" : "false"}
             /*onChange={(e) => setAge(e.target.value)}
           */
-          >
-           
-          </Dropdown>
-          {/*{errors.age?.type === "required" && (
+          />
+          {/* {errors.age?.type === "required" && (
             <p role="alert">This field is required</p>
-          )}*/}
+          )} */}
         </div>
 
-        <div className="flex-col gap-28 py-20 px-40  text-xl" id="email">
+        <div className="flex-col gap-28 py-20 px-40 text-xl" id="email">
           <div className="relative font-bold">Im registering as a ...</div>
 
           <Dropdown
             options={roleOptions}
             placeholder="Select an option..."
             className="w-1/3 rounded"
-          ></Dropdown>
+          />
         </div>
 
         <div className="flex-col gap-28 py-20 px-40  text-xl" id="email">
-          <div className="relative font-bold">
-            Do you have any dietary restrictions?
-          </div>
+          <div className="relative font-bold">Dietary restrictions</div>
 
           <Dropdown
             options={dietOptions}
             placeholder="Select an option..."
             className="w-1/3 rounded"
-          ></Dropdown>
+          />
         </div>
 
         <div className="flex-col gap-28 py-20 px-40  text-xl" id="email">
-          <div className="relative font-bold">What is your referral code?</div>
+          <div className="relative font-bold">Referral Code</div>
 
           <input
             className="bg-transparent h-10 w-1/2 rounded shadow"
             type="text"
             placeholder=" Type your answer here..."
-          ></input>
+          />
         </div>
 
         <div className="flex-col gap-28 py-20 px-40  text-xl" id="email">
@@ -178,19 +175,19 @@ const Application = () => {
             options={whereOptions}
             placeholder="Select an option..."
             className="w-1/3 rounded"
-          ></Dropdown>
+          />
         </div>
 
-        <div className="flex-col gap-28 py-20 px-40  text-xl" id="email">
+        <div className="flex-col gap-28 py-20 px-40 text-xl" id="email">
           <div className="relative font-bold">
-            Do you agree with the MLH terms and conditions?
+            Do you agree to the MLH terms and conditions?
           </div>
 
           <Dropdown
             options={agreeOptions}
             placeholder="Select an option..."
             className="w-1/3 rounded"
-          ></Dropdown>
+          />
         </div>
 
         <div className="gap-28 py-20 px-40">
