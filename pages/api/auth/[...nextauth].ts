e3410a2402264fe96dcc1b1c2b94e306ad115904
Tyler Nguyen <tytyprime@gmail.com>
2023-01-17 17:11:54 -0500

    add thing to credential thing

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
 
 export const authOptions = {
   providers: [
@@ -11,6 +13,17 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CredentialsProvider({
+      type: "credentials",
+      credentials: {
+        username: { label: "username", type: "text", placeholder: "deeznuts@gmail.com" },
+        password: { label: "password", type: "password" },
+      },
+      authorize: async (credentials, req) => {
+        console.log("deez nuts");
+            return null;
+      },
     }),
   ],
   pages: {
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,8 @@
+import { sign } from "crypto";
 import { InferGetServerSidePropsType } from "next";
 import { getProviders, signIn } from "next-auth/react";
+import Image from "next/image";
+import { userInfo } from "os";
 
 // add github and custom email/username pass
 const Login = ({
@@ -7,6 +10,22 @@ const Login = ({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <div className="text-center">
+          <div className="relative z-50 mx-auto aspect-square h-24">
+            <Image src="/logo-colored.png" alt="Hack the Nest Logo" fill />
+          </div>
+          <input
+            onChange={() => signIn()}
+            type="text"
+            placeholder="email"
+          />
+          <div>
+            <button onClick={() => signIn()}>sign in</button>
+          </div>
+        </div>
+      </div>
+
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
           <button
