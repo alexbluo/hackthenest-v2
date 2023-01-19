4553a01a41fd0f2d3c1ab13627e6126476fe5930
Alex Luo <alexbluo01@gmail.com>
2023-01-19 00:25:01 -0500

    add background

--- components/FrontSection.tsx
+++ components/FrontSection.tsx
@@ -1,20 +1,44 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const FrontSection = () => {
   return (
     <section className="h-screen pt-32" id="front">
-      <div className="flex h-full flex-col items-center justify-center">
-        <h1 className="pb-4 font-header text-6xl font-extrabold">
+      <div className="absolute top-0 left-0 h-screen w-screen">
+        <Image
+          className="object-cover"
+          src="/background.png"
+          alt="placeholder"
+          fill
+        />
+      </div>
+      <div className="flex flex-col items-center justify-center pt-16">
+        <h1 className="z-50 pb-4 font-header text-6xl font-extrabold">
           Hack the Nest
         </h1>
-        <p className="">April 15-16, 2023</p>
-        <p className="">River Hill High School</p>
-        <Link href="/dashboard">
-          <button className="rounded-lg border py-4 px-12">
-            Apply Now
-          </button>
+        <p className="z-50">April 15-16, 2023</p>
+        <p className="z-50">River Hill High School</p>
+        <Link
+          className="z-50 mx-auto rounded-lg border py-4 px-12"
+          href="/dashboard"
+        >
+          Apply Now
         </Link>
       </div>
+
+      <a
+        id="mlh-trust-badge"
+        className="absolute right-4 top-0 z-50 hidden aspect-[571/1000] w-[10%] min-w-[60px] max-w-[100px] lg:block "
+        href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2023-season&utm_content=yellow"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          src="https://s3.amazonaws.com/logged-assets/trust-badge/2023/mlh-trust-badge-2023-yellow.svg"
+          alt="Major League Hacking 2023 Hackathon Season"
+          fill
+        />
+      </a>
     </section>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,19 +21,6 @@ const Home: NextPage = () => {
       </Head>
       <div className={classNames({ "fixed left-0 right-0 md:static": open })}>
         <NavBar open={open} toggleOpen={toggleOpen} />
-        <a
-          id="mlh-trust-badge"
-          className="absolute right-4 top-0 z-50 hidden aspect-[571/1000] w-[10%] min-w-[60px] max-w-[100px] lg:block "
-          href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2023-season&utm_content=yellow"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Image
-            src="https://s3.amazonaws.com/logged-assets/trust-badge/2023/mlh-trust-badge-2023-yellow.svg"
-            alt="Major League Hacking 2023 Hackathon Season"
-            fill
-          />
-        </a>
         <main>
           <FrontSection />
           <AboutSection />
