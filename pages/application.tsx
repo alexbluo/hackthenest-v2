bd8c026258927200baa2ed49a8e244274eb31dcb
Evan Marlow <itsarlow@gmail.comgit config --global user.email itsarlow@gmail.com>
2023-01-19 18:36:16 -0500

    made changes to application

--- pages/application.tsx
+++ pages/application.tsx
@@ -1,7 +1,8 @@
-import Dropdown from "react-dropdown";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import "react-dropdown/style.css";
+import { METHODS } from "http";
+import { useForm, Controller } from "react-hook-form";
+import Select from "react-select";
+import { optionCSS } from "react-select/dist/declarations/src/components/Option";
 
 // TODO: move options to inline
 // fix other - search up
@@ -17,12 +18,17 @@ import "react-dropdown/style.css";
 
 import * as z from "zod";
 
+interface Option {
+  value: string;
+  label: string;
+}
+
 const schema = z.object({
-  firstName: z.string().nonempty('Field is required'),
-  lastName: z.string().nonempty('Field is required'),
-  email: z.string().nonempty('Field is required'),
-  phoneNumber: z.string().nonempty('Field is required'),
-  
+  firstName: z.string().min(1, { message: "Field is required" }),
+  lastName: z.string().min(1, { message: "Field is required" }),
+  email: z.string().min(1, { message: "Field is required" }),
+  phoneNumber: z.string().min(1, { message: "Field is required" }),
+  // accept: z.boolean({message: "Field is required"})
 });
 
 const Application = () => {
@@ -30,170 +36,186 @@ const Application = () => {
     register,
     handleSubmit,
     formState: { errors },
-    // watch,
+    control,
   } = useForm({
     resolver: zodResolver(schema),
   });
+
+  const ageOptions: Option[] = [
+    { value: "13", label: "13" },
+    { value: "14", label: "14" },
+    { value: "15", label: "15" },
+    { value: "16", label: "16" },
+    { value: "17", label: "17" },
+    { value: "18", label: "18" },
+  ];
+
+  const dietOptions: Option[] = [
+    { value: "Vegan", label: "Vegan" },
+    { value: "Vegetarian", label: "Vegetarian" },
+    { value: "Peanut allergies", label: "Peanut allergies" },
+    // <input placeholder="Other..." />,
+  ];
+
+  const outreachOptions: Option[] = [
+    { value: "Friends/Family", label: "Friends/Family" },
+    { value: "LinkedIn", label: "LinkedIn" },
+    { value: "Instagram", label: "Instagram" },
+    { value: "Twitter", label: "Twitter" },
+    { value: "Facebook", label: "Facebook" },
+  ];
 
   const onSubmit = (data) => {
     console.log(data);
   };
 
   return (
-    <section className="h-[160rem]">
+    <section className="h-[120rem]">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex gap-28 py-20 px-40 text-xl">
-          <div className="w-1/2 flex-col">
-            <div className="relative font-bold">First Name *</div>
+        <div className="flex flex-col gap-12">
+          <div className="flex gap-28 px-40 text-xl">
+            <div className="w-1/2 flex-col">
+              <div className="relative font-bold">First Name *</div>
 
+              <input
+                className="bg-transparent h-10 w-full rounded shadow"
+                type="text"
+                placeholder=" Type your answer here..."
+                {...register("firstName")}
+              />
+              {errors.firstName && <p>{errors.firstName.message}</p>}
+            </div>
+
+            <div className="w-1/2 flex-col">
+              <div className="relative font-bold">Last Name *</div>
+              <input
+                className="bg-transparent h-10 w-full rounded shadow"
+                type="text"
+                placeholder=" Type your answer here..."
+                {...register("lastName")}
+              />
+              {errors.lastName && <p>{errors.lastName.message}</p>}
+            </div>
+          </div>
+
+          <div className="flex-col gap-28 px-40 text-xl">
+            <div className="font-bold">Email *</div>
             <input
               className="bg-transparent h-10 w-full rounded shadow"
               type="text"
               placeholder=" Type your answer here..."
-              {...register("firstName")}
+              {...register("email")}
             />
-            {errors.firstName?.message && <p>{errors.firstName?.message}</p>}
+            {errors.email && <p>{errors.email.message}</p>}
           </div>
 
-          <div className="w-1/2 flex-col">
-            <div className="relative font-bold">Last Name *</div>
+          <div className="flex-col gap-28 px-40  text-xl">
+            <div className="font-bold">Phone Number *</div>
             <input
               className="bg-transparent h-10 w-full rounded shadow"
               type="text"
               placeholder=" Type your answer here..."
-              {...register("lastName")}
+              {...register("phoneNumber")}
             />
-            {errors.lastName?.message && <p>{errors.lastName?.message}</p>}
+            {errors.phoneNumber && <p>{errors.phoneNumber.message}</p>}
           </div>
-        </div>
 
-        <div className="flex-col gap-28 py-20 px-40 text-xl">
-          <div className="font-bold">Email *</div>
-          <input
-            className="bg-transparent h-10 w-full rounded shadow"
-            type="text"
-            placeholder=" Type your answer here..."
-            {...register("email")}
-          />
-          {errors.email?.message && <p>{errors.email?.message}</p>}
-        </div>
-
-        <div className="flex-col gap-28 py-20 px-40  text-xl">
-          <div className="font-bold">Phone Number *</div>
-          <input
-            className="bg-transparent h-10 w-full rounded shadow"
-            type="text"
-            placeholder=" Type your answer here..."
-            {...register("phoneNumber")}
-          />
-          {errors.phoneNumber?.message && <p>{errors.phoneNumber?.message}</p>}
-        </div>
-
-        <div className="flex-col gap-28 py-20 px-40 text-xl">
-          <div className="font-bold">Age *</div>
-          <Dropdown
-            options={["13", "14", "15", "16", "17", "18"]}
-            placeholder="Select an option..."
-            className="w-1/3 rounded"
-
-            /* {...register("age", {
-              required: true,
-            })}
-            aria-invalid={errors.age ? "true" : "false"}
-            /*onChange={(e) => setAge(e.target.value)}
-          */
-          />
-          {/* {errors.age?.type === "required" && (
-            <p role="alert">This field is required</p>
-          )} */}
-        </div>
-
-        <div className="flex-col gap-28 py-20 px-40 text-xl">
-          <div className="relative font-bold">Dietary restrictions</div>
-          <Dropdown
-            options={[
-              "Vegan",
-              "Vegetarian",
-              "Peanut allergies",
-              // <input placeholder="Other..." />,
-            ]}
-            placeholder="Select an option..."
-            className="w-1/3 rounded"
-          />
-        </div>
-
-        <div className="flex-col gap-28 py-20 px-40 text-xl">
-          <div className="font-bold">Referral Code</div>
-          <input
-            className="bg-transparent h-10 w-1/2 rounded shadow"
-            type="text"
-            placeholder=" Type your answer here..."
-          />
-        </div>
-
-        <div className="flex-col gap-28 py-20 px-40 text-xl">
-          <div className="font-bold">Where did you hear about us?</div>
-          <Dropdown
-            options={[
-              "Friends/Family",
-              "LinkedIn",
-              "Instagram",
-              "Twitter",
-              "Facebook",
-              // <input placeholder="Other..." />,
-            ]}
-            placeholder="Select an option..."
-            className="w-1/3 rounded"
-          />
-        </div>
-
-        <div className="flex-col gap-28 py-20 px-40 text-xl">
-          <div className="font-bold">
-            I have read and agree to the&nbsp;
-            <a
-              href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
-              target="_blank"
-              rel="noreferrer"
-            >
-              MLH Code of Conduct
-            </a>
+          <div className="flex-col gap-28 px-40 text-xl">
+            <div className="font-bold">Age *</div>
+            <Select
+              options={ageOptions}
+              className="w-1/3 rounded"
+             
+            />
           </div>
-          <Dropdown
-            options={agreeOptions}
-            placeholder="Select an option..."
-            className="w-1/3 rounded"
-          />
-        </div>
 
-        <div className="flex-col gap-28 py-20 px-40 text-xl">
-          <div className="font-bold">
-            I authorize you to share my application/registration information
-            with Major League Hacking for event administration, ranking, and MLH
-            administration in-line with the&nbsp;
-            <a href="https://mlh.io/privacy" target="_blank" rel="noreferrer">
-              MLH Privacy Policy
-            </a>
-            . I further agree to the terms of both the&nbsp;
-            <a
-              href="https://github.com/MLH/mlh-policies/blob/main/contest-terms.md"
-              target="_blank"
-              rel="noreferrer"
-            >
-              MLH Contest Terms and Conditions
-            </a>
-            and the MLH Privacy Policy.
+          <div className="flex-col gap-28 px-40 text-xl">
+            <div className="relative font-bold">Dietary restrictions</div>
+            <Controller
+              control={control}
+              defaultValue="Select an option..."
+              name="dietField"
+              render={({ field: { onChange, value, ref } }) => (
+                <Select
+                  ref={ref}
+                  options={dietOptions}
+                  placeholder="Select an option..."
+                  className="w-1/3 rounded"
+                  value={dietOptions.find((el) => el.value === value)}
+                  onChange={(newValue) => onChange(newValue.value)}
+                />
+              )}
+            />
           </div>
-          <Dropdown
-            options={agreeOptions}
-            placeholder="Select an option..."
-            className="w-1/3 rounded"
-          />
-        </div>
 
-        <div className="gap-28 py-20 px-40">
-          <button className="rounded-lg border px-12 py-4 hover:bg-orange">
-            Submit
-          </button>
+          <div className="flex-col gap-28 px-40 text-xl">
+            <div className="font-bold">Referral Code</div>
+            <input
+              className="bg-transparent h-10 w-1/2 rounded shadow"
+              type="text"
+              placeholder=" Type your answer here..."
+            />
+          </div>
+
+          <div className="flex-col gap-28 px-40 text-xl">
+            <div className="font-bold">Where did you hear about us?</div>
+            <Select
+              options={outreachOptions}
+              placeholder="Select an option..."
+              className="w-1/3 rounded"
+            />
+          </div>
+
+          <div className="flex-col gap-28 px-40 text-xl">
+            <div className="font-bold">
+              I have read and agree to the&nbsp;
+              <a
+                href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+                target="_blank"
+                rel="noreferrer"
+              >
+                MLH Code of Conduct
+              </a>
+            </div>
+            <input
+              placeholder="Select an option..."
+              className="h-6 w-6 rounded"
+              type="checkbox"
+            />
+          </div>
+
+          <div className="flex-col gap-28 px-40 text-xl">
+            <div className="font-bold">
+              I authorize you to share my application/registration information
+              with Major League Hacking for event administration, ranking, and
+              MLH administration in-line with the&nbsp;
+              <a href="https://mlh.io/privacy" target="_blank" rel="noreferrer">
+                MLH Privacy Policy
+              </a>
+              . I further agree to the terms of both the&nbsp;
+              <a
+                href="https://github.com/MLH/mlh-policies/blob/main/contest-terms.md"
+                target="_blank"
+                rel="noreferrer"
+              >
+                MLH Contest Terms and Conditions
+              </a>
+              and the MLH Privacy Policy.
+            </div>
+            <input
+              placeholder="Select an option..."
+              className="h-6 w-6 rounded"
+              type="checkbox"
+              {...register("accept")}
+            />
+            {errors.accept && <p>{errors.accept.message}</p>}
+          </div>
+
+          <div className="gap-28 px-40">
+            <button className="rounded-lg border px-12 py-4 hover:bg-orange">
+              Submit
+            </button>
+          </div>
         </div>
       </form>
     </section>
