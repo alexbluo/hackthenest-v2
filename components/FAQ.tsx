a97b5beeb78ee003ca88e1a148822fb8fab89de6
Alex Luo <alexbluo01@gmail.com>
2023-02-13 20:29:33 -0500

    change faq easing

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -11,13 +11,20 @@ const FAQ = ({ question, children }: Props) => {
 
   return (
     <div className="flex flex-col">
-      <h3 className="cursor-pointer text-xl select-none" onClick={() => setOpen(!open)}>
+      <h3
+        className="cursor-pointer select-none text-xl"
+        onClick={() => setOpen(!open)}
+      >
         {question}
       </h3>
       <motion.div
-        className="overflow-y-hidden text-md pt-2"
+        className="text-md overflow-y-hidden pt-2"
         animate={open ? "open" : "closed"}
         variants={{ closed: { height: 0 }, open: { height: "auto" } }}
+        transition={{
+          ease: "easeIn",
+          duration: 0.2,
+        }}
       >
         {children}
       </motion.div>
