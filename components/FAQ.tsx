6ca02525b75ed849931ac4466c8bed0712f2242a
Alex Luo <alexbluo01@gmail.com>
2023-02-13 20:50:44 -0500

    add faq open close svg rotate and collapse samples

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -1,6 +1,17 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 
+const Path = (props: any) => {
+  return (
+    <motion.path
+      className="stroke-white"
+      strokeWidth="3"
+      strokeLinecap="round"
+      {...props}
+    />
+  );
+};
+
 interface Props {
   question: string;
   children: string;
@@ -11,12 +22,36 @@ const FAQ = ({ question, children }: Props) => {
 
   return (
     <div className="flex flex-col">
-      <h3
-        className="cursor-pointer select-none text-xl"
+      <button
+        className="flex cursor-pointer select-none items-center justify-between text-xl"
         onClick={() => setOpen(!open)}
       >
-        {question}
-      </h3>
+        <h3 className="cursor-pointer select-none text-xl">{question}</h3>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          height="24"
+          width="24"
+        >
+          <Path d="M 4 12 L 20 12" />
+          <Path
+            animate={open ? "open" : "closed"}
+            d="M 12 4 L 12 20"
+            // variants={{
+            //   closed: { rotate: 0 },
+            //   open: { rotate: 90 },
+            // }}
+            variants={{
+              closed: { d: "M 12 4 L 12 20" },
+              open: { d: "M 12 12 L 12 12" },
+            }}
+            transition={{
+              ease: "easeIn",
+              duration: 0.2,
+            }}
+          />
+        </svg>
+      </button>
       <motion.div
         className="text-md overflow-y-hidden pt-2"
         animate={open ? "open" : "closed"}
