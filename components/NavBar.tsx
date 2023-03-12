04b3786d72f2ae541bfcd67be73a40feb01833a8
Alex Luo <alexbluo01@gmail.com>
2023-03-12 18:31:01 -0400

    add dashboard login to nav

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 import MobileMenu from "./MobileMenu";
 import MobileMenuToggle from "./MobileMenuToggle";
 import NavItem from "./NavItem";
@@ -10,10 +12,17 @@ interface Props {
 }
 
 const NavBar = ({ open, toggleOpen }: Props) => {
+  const { data: session } = useSession();
+
   return (
-    <nav className="bg-transparent container absolute top-0 left-0 right-0 z-50 flex h-32 w-full items-center justify-between">
+    <nav className="container absolute top-0 left-0 right-0 z-50 flex h-32 w-full items-center justify-between bg-transparent">
       <div className="relative z-50 aspect-square h-3/5">
-        <Image src="/logo-colored.png" alt="Hack the Nest Logo" sizes="76.8px" fill />
+        <Image
+          src="/logo-colored.png"
+          alt="Hack the Nest Logo"
+          sizes="76.8px"
+          fill
+        />
       </div>
 
       <ul className="hidden gap-12 text-gold sm:flex">
@@ -21,6 +30,12 @@ const NavBar = ({ open, toggleOpen }: Props) => {
         <NavItem to="schedule">Schedule</NavItem>
         <NavItem to="faq">FAQ</NavItem>
         <NavItem to="sponsors">Sponsors</NavItem>
+        <Link
+          className="cursor-pointer font-header text-lg font-medium"
+          href={session ? "/dashboard" : "/login"}
+        >
+          {session ? "Dashboard" : "Login"}
+        </Link>
       </ul>
 
       <MobileMenuToggle open={open} handleClick={() => toggleOpen()} />
