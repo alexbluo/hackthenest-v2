e062c8d75ebcce5e56656c4375453ecaa51e6bf6
Alex Luo <alexluo92823@gmail.com>
2023-06-29 19:43:52 -0400

    fix stat modal open bug

--- app/sections/AboutSection/Stat.tsx
+++ app/sections/AboutSection/Stat.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import useWindowWidth from "utils/useWindowWidth";
 import Modal from "../../components/Modal";
 
@@ -10,25 +10,26 @@ interface Props {
   children: ReactNode;
 }
 
-// TODO: add modal?
 const Stat = ({ stat, caption, children }: Props) => {
   const [open, setOpen] = useState<boolean>(false);
   const width = useWindowWidth();
 
   return (
-    <button
-      className="gradient-bg h-36 w-full cursor-pointer rounded-xl bg-white px-4 py-10 text-center text-black shadow-md shadow-black duration-200 ease-in-out hover:shadow-lg hover:shadow-blue-light"
-      onClick={() => setOpen(true)}
-    >
-      <h3 className="pb-2 text-4xl font-semibold">{stat}</h3>
-      <h4 className="">{caption}</h4>
+    <>
+      <button
+        className="gradient-bg h-36 w-full cursor-pointer rounded-xl bg-white px-4 py-10 text-center text-black shadow-md shadow-black duration-200 ease-in-out hover:shadow-lg hover:shadow-blue-light"
+        onClick={() => setOpen(true)}
+      >
+        <h3 className="pb-2 text-4xl font-semibold">{stat}</h3>
+        <h4 className="">{caption}</h4>
+      </button>
       <Modal visible={open} width={width} onTap={() => setOpen(false)}>
         <h3 className="mb-2 text-3xl font-semibold">
           {stat} {caption}
         </h3>
         <p>{children}</p>
       </Modal>
-    </button>
+    </>
   );
 };
 
